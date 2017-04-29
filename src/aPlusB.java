602d7c40cf4bb9cac62df4f65874447225364561
anayi89 <it.garry@gmail.com>
2017-04-29 15:57:31 -0400

    Update and rename aAndB.java to src/aPlusB.java

--- src/aPlusB.java
+++ src/aPlusB.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class aAndB {  
+public class aPlusB {  
 	public static void main(String[] args) {
 		JFrame a = new JFrame("A + B");
 
