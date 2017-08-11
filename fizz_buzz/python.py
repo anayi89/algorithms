f60fed4f72078c4edf03f715247526c9065a13b2
anayi89 <it.garry@gmail.com>
2017-08-10 20:28:06 -0400

    Create python.py

---
+++ fizz_buzz/python.py
@@ -0,0 +1,12 @@
+for i in range(1, 101):
+
+	if (i % 3 == 0):
+		print(“Fizz”, end = “ ”)
+
+	if (i % 5 == 0):
+		print(“Buzz”, end = “ ”)
+
+	if (i % 3 != 0) and (i % 5 != 0):
+		print(i, end = “ ”)
+
+	print()
