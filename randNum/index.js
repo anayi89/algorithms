b6a182472fa6d0432eef34aac8aa2ad403ec15b8
anayi89 <it.garry@gmail.com>
2017-08-13 18:18:37 -0400

    Create index.js

---
+++ randNum/index.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    </head>
+    
+    <body>
+    	<p id="text"></p>
+    	<script type="text/javascript" charset="utf-8">
+		function doors_100() {
+			for(var i = 1; i <= 100; i++) {
+				if(i % 3 == 0 && i % 5 == 0) {
+					if(i % 3 == 0) {
+					document.getElementById("text").innerHTML += "Fizz";
+					}
+					if(i % 5 == 0) {
+					document.getElementById("text").innerHTML += " Buzz";
+					}
+				}
+				else if(i % 3 == 0 && i % 5 !== 0) {
+					document.getElementById("text").innerHTML += "Fizz";
+				}
+				else if(i % 3 !== 0 && i % 5 == 0) {
+					document.getElementById("text").innerHTML += "Buzz";
+				}
+				else {
+					document.getElementById("text").innerHTML += i;
+				}
+				document.getElementById("text").innerHTML += "<br />";
+			}
+		}
+		    doors_100();
+        </script>
+    </body>
+</html>
