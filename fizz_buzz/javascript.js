545011453d4f38164df468fb07bb78f3299083c8
anayi89 <it.garry@gmail.com>
2017-08-13 13:00:50 -0400

    Update javascript.js

--- fizz_buzz/javascript.js
+++ fizz_buzz/javascript.js
@@ -7,20 +7,29 @@
     <body>
     	<p id="text"></p>
     	<script type="text/javascript" charset="utf-8">
-		    function doors_100() {
-			    for(var i = 1; i <= 100; i++) {
-				    if(i % 3 == 0) {
-				        document.getElementById("text").innerHTML += "Fizz" + "<br />";
-				    }
-				    else if(i % 5 == 0) {
-				        document.getElementById("text").innerHTML += "Buzz" + "<br />";
-				    }
-				    else {
-				        document.getElementById("text").innerHTML += i + "<br />";
-				    }
-			    }
-		    }
-		    doors_100();
+		function doors_100() {
+			for(var i = 1; i <= 100; i++) {
+				if(i % 3 == 0 && i % 5 == 0) {
+					if(i % 3 == 0) {
+					document.getElementById("text").innerHTML += "Fizz";
+					}
+					if(i % 5 == 0) {
+					document.getElementById("text").innerHTML += " Buzz";
+					}
+				}
+				else if(i % 3 == 0 && i % 5 !== 0) {
+					document.getElementById("text").innerHTML += "Fizz";
+				}
+				else if(i % 3 !== 0 && i % 5 == 0) {
+					document.getElementById("text").innerHTML += "Buzz";
+				}
+				else {
+					document.getElementById("text").innerHTML += i;
+				}
+				document.getElementById("text").innerHTML += "<br />";
+			}
+		}
+		doors_100();
         </script>
     </body>
 </html>
