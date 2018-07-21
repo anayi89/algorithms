1fdc6df95fbbe7b29a98b923787d7dae2112fe15
anayi89 <it.garry@gmail.com>
2018-07-21 16:16:00 -0400

    Update python.py

--- fizz_buzz/python.py
+++ fizz_buzz/python.py
@@ -1,12 +1,11 @@
-for i in range(1, 101):
-
-	if (i % 3 == 0):
-		print(“Fizz”, end = “ ”)
-
-	if (i % 5 == 0):
-		print(“Buzz”, end = “ ”)
-
-	if (i % 3 != 0) and (i % 5 != 0):
-		print(i, end = “ ”)
-
-	print()
+def fizzBuzz():
+    for i in range(1, 21):
+        if (i % 3 == 0) and (i % 5 == 0):
+            print('Fizz Buzz')
+        elif (i % 3 == 0):
+            print('Fizz')
+        elif (i % 5 == 0):
+            print('Buzz')
+        else:
+            print(i)
+fizzBuzz()
